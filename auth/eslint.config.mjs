02d0c60d3d27f6cee2dd87c83ca1afd73f72caf8
Sumit Sehgal <sumitshagal12@gmail.com>
2026-07-19 16:24:42 +0530

    chore: ignore env files and fix workspace lint configuration

--- auth/eslint.config.mjs
+++ auth/eslint.config.mjs
@@ -8,7 +8,12 @@ export default [
       '@nx/dependency-checks': [
         'error',
         {
-          ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}'],
+          ignoredFiles: [
+            '{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}',
+          ],
+          ignoredDependencies: [
+            'pino-pretty',
+          ],
         },
       ],
     },
