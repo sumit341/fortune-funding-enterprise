6169998576551ac48860809b9d4baa7d8473bce5
Sumit Sehgal <sumitshagal12@gmail.com>
2026-07-27 14:48:56 +0530

    fix frontend router and page structure

---
+++ frontend/src/app/router.tsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import Admin from "../pages/Admin/Admin";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/admin",
+    element: <Admin />,
+  },
+]);
---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Fortune Funding</h1>
+      <p>Home Page</p>
+    </div>
+  );
+}
